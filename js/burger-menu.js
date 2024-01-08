d6baa797298de7bf19504c924f904f78594fe56b
PedroRibot <62023844+PedroRibot@users.noreply.github.com>
2024-01-08 10:38:59 +0100

    Update burger-menu.js

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,13 +1,15 @@
 var menu_burger = document.getElementById("menu-burger");
 var menu_nav = document.getElementById("menu-nav");
 
-menu_burger.onclick = function() { toggleEvent(); };
+if (menu_burger !== null) {  
+    menu_burger.onclick = function() { toggleEvent(); };
 
-function toggleEvent() {
-    menu_burger.classList.toggle('clicked');
-    menu_nav.classList.toggle('show');
-}
-
-function closeBurger() {
-    menu_nav.classList.remove('show');
+    function toggleEvent() {
+        menu_burger.classList.toggle('clicked');
+        menu_nav.classList.toggle('show');
+    }
+    
+    function closeBurger() {
+        menu_nav.classList.remove('show');
+    }
 }
